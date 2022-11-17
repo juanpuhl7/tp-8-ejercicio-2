7157ea943919b4554a5ad82d9efcaf94cd7a7c77
johnmaster <juanpuhl404@gmail.com>
2022-11-16 22:20:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CANTDIAS=5;
+
+            string [] arreglos_Dias ={"Lunes","Martes","Miercoles","Jueves","Viernes"};
+
+          
+            Console.WriteLine("DIAS DE LA SEMANA");
+            for(int i=0; i<CANTDIAS; i ++){
+                   Console.WriteLine(arreglos_Dias[i]);   
+            }
+            
+            Console.WriteLine("DIAS DE LA SEMANA AL REVES");
+            for(int i=4; i>=0; i --){
+                   Console.WriteLine(arreglos_Dias[i]);   
+            }
+          
+          
+          Console.WriteLine("Presione una tecla para terminar...");
+          Console.ReadKey();
+
+        }
+    }
+}
